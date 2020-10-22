30c67aac13aae9b25bd0ef4acf8f422647a410dd
aabaroab <72941918+aabaroab@users.noreply.github.com>
2020-10-22 16:40:51 +0200

    Add files via upload

---
+++ funtcionA.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+    </head>
+    <body>
+        <?php
+            function writeMsg() {
+            echo "Hello world!";
+        }
+        writeMsg();
+        ?>
+    </body>
+</html>
